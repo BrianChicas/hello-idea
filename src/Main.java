2b9628d24705cef62a36c1d618224453a52f38f1
Brian Chicas-pineda <Chicasbrian710@gmail.com>
2025-01-22 12:54:58 -0800

    adds todays date

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Idea!");
+        System.out.println("Hello Idea! Today is 1/22/2025 ");
     }
 }
