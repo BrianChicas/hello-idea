d0ebd2cec776f5c1e6dd698fc869fb2037cf4664
Brian Chicas-pineda <Chicasbrian710@gmail.com>
2025-01-22 13:38:44 -0800

    adds code from class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+//Brian Chicas
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Idea! Today is 1/22/2025 ");
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is your name?\n");
+        String name = scanner.nextLine();
+        System.out.print("What is your favorite number?\n");
+
+        Double num = 0d;
+
+        try {
+        num = scanner.nextDouble();
+        } catch (Exception e) {
+            System.out.printf(num + " is not a number");
+            System.exit(0);
+        }
+        //double num = scanner.nextDouble();
+
+        System.out.println("Hi " + name + "! " + num + " is a great number!");
+    } // End constructor
 }
